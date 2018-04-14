4752998ce204735194f709accf20cf9bf2c0a608
baixiaocheng <863562441@qq.com>
2018-04-15 01:08:49 +0800

    新建course仓库

---
+++ php/note.js
@@ -0,0 +1,20 @@
+Hypertext Preprocessor
+1、服务器端脚本
+2、运行：服务器组件（WampServer, XAMPP）
+3、IDE （Eclipse for PHP）
+4、语法
+	<?php代码?>
+	输出指令: echo(可输出任意个字符串), print(输出一个字符串,返回1)
+5、	变量: $变量名
+	弱类型
+	变量作用域: local, global($GLOBAL[index]), static, parameter
+6、定界符EOF: 定义字串方法
+7、数据类型: 
+	String, Integer, Float, Boolean, Array, Object(声明), NULL
+	var_dump(): 返回类型、长度和值
+8、常量
+	define();
+9、字符串
+	并置运算符.
+	strlen();
+	strpos();
