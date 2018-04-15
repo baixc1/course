2db4ad2d261c9eaf7b6dad4c32b6df52ff58d343
baixiaocheng <863562441@qq.com>
2018-04-15 19:28:29 +0800

    the word of 4.15

--- php/note.js
+++ php/note.js
@@ -18,3 +18,37 @@ Hypertext Preprocessor
 	并置运算符.
 	strlen();
 	strpos();
+10、array()
+	数值数组、关联数组、多维数组
+	count()
+	for循环, foreach(arr as val)循环
+11、数组排序
+	sort,rsort,asort,ksort,arsort,krsort
+	print_r: 打印数组
+12、超级全局变量
+	superglobals($GLOBAL, $_SREVER, $_REQUEST, $_POST, $_GET)
+13、魔术变量
+	__LINE__, __FILE__, __DIR__, __FUNCTION__, __CLASS__, __METHOD__, __NAMESPACE__
+14、命名空间
+15、对象
+	父类(基类,超类),子类(派生类) 
+	class phpClass{
+		var $x = 1;
+		public function __construct(){}
+		function fun(){}
+	}
+	类成员访问 ->
+	继承 extends
+	访问控制 public, protected, private
+	接口 interface, 实现接口 class className implements interfaceName
+	抽象类 abstract 子类继承
+	static
+	final
+
+
+
+	
+		
+	
+	
+	
---
+++ thinkphp/note.js
@@ -0,0 +1,5 @@
+thinkphp
+1、PHP开发框架
+2、github下载think(目录结构)和framework(一级子目录phpthink)
+3、入口public/index.php
+4、安装php, php think bulid --module demo
