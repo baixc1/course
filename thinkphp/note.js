5ffeabe5081e888781d45ca79e86d85c2e4a5870
baixc <863562441@qq.com>
2018-04-16 18:52:13 +0800

    thinkphp 4.16

--- thinkphp/note.js
+++ thinkphp/note.js
@@ -3,3 +3,5 @@ thinkphp
 2、github下载think(目录结构)和framework(一级子目录phpthink)
 3、入口public/index.php
 4、安装php, php think bulid --module demo
+5、浏览器错误乱码: echo iconv('gbk', 'utf-8', $e->getMessage());
+6、拷贝项目打开报错: 修改mysql配置(data\conf)
