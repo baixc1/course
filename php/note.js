ffc487c3bdb550bb631ecfd7ef89c9637de541b3
baixc <863562441@qq.com>
2018-04-17 18:40:39 +0800

    the course of april 17

--- php/note.js
+++ php/note.js
@@ -5,6 +5,8 @@ Hypertext Preprocessor
 4、语法
 	<?php代码?>
 	输出指令: echo(可输出任意个字符串), print(输出一个字符串,返回1)
+	.号: 连接符
+	解析双引号内的php变量
 5、	变量: $变量名
 	弱类型
 	变量作用域: local, global($GLOBAL[index]), static, parameter
@@ -19,17 +21,23 @@ Hypertext Preprocessor
 	strlen();
 	strpos();
 10、array()
-	数值数组、关联数组、多维数组
+	数值数组、关联数组array(''=>'',..)、多维数组
 	count()
 	for循环, foreach(arr as val)循环
+	
 11、数组排序
 	sort,rsort,asort,ksort,arsort,krsort
 	print_r: 打印数组
 12、超级全局变量
 	superglobals($GLOBAL, $_SREVER, $_REQUEST, $_POST, $_GET)
 13、魔术变量
-	__LINE__, __FILE__, __DIR__, __FUNCTION__, __CLASS__, __METHOD__, __NAMESPACE__
+	__LINE__, 
+	__FILE__: 文件的完整路径和文件名
+	, __DIR__, __FUNCTION__, __CLASS__, __METHOD__, __NAMESPACE__
 14、命名空间
+	namespace xx\xxx
+	作用: 1、类、函数、常量名字冲突 2、提供别名, 提高代码可读性
+	use class 引入类库
 15、对象
 	父类(基类,超类),子类(派生类) 
 	class phpClass{
@@ -44,11 +52,16 @@ Hypertext Preprocessor
 	抽象类 abstract 子类继承
 	static
 	final
+16、函数
+	define();		//定义常量
 
 
 
+advanced course
+1、PHP包含
+	include,require(错误中断) : 引用页头、公用文件、变量
 	
-		
-	
+reference manual
+1、dirpath() 返回目录路径	
 	
 	
--- thinkphp/note.js
+++ thinkphp/note.js
@@ -4,5 +4,10 @@ thinkphp
 3、入口public/index.php
 4、安装php, php think bulid --module demo
 5、浏览器错误乱码: echo iconv('gbk', 'utf-8', $e->getMessage());
-6、拷贝项目打开报错: 修改mysql配置(data\conf)
+6、服务器项目打开报错: 
+		\kilimanjaro\.htaccess RewriteRule才能在localhost看到文件
+		failed to open stream  	index.php 开启调试模式
+		Access denied for user 'root'@'localhost'   data/conf/db.php 修改DB_PWD,DB_NAME 刷新页面
+		data/runtime/Logs/Portal/.log   错误日志
+		php打印log: file_put_contents('文件根目录',"");
 7、mysql打不开(服务名无效): mysqld --install
