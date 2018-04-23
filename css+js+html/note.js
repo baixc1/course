72bdd9b38464db119cf7c69c23b748c189fd95cf
baixc <863562441@qq.com>
2018-04-23 18:49:31 +0800

    the note of course in  April twenty-three

---
+++ css+js+html/note.js
@@ -0,0 +1,6 @@
+1、多个background用','号隔开,蒙层优先级高->低
+2、margin: auto; 对块标签元素有效，作用于自身
+	text-align: center; 对行内元素/文本有效，作用域子元素
+3、background-size: len,len;	设置多个背景的宽高
+4、list-style(ul的简写属性) 设置列表项标志
+5、nth-child(n)  先找某元素的父元素，然后找第n个(递归查找)子元素且元素必须为原元素
