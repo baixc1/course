0cd5147b9b02ea68bb5b7d3d68bae96d6c1c4124
baixiaocheng <863562441@qq.com>
2018-05-02 22:58:42 +0800

    add html5.js

---
+++ css+html/html5.js
@@ -0,0 +1,21 @@
+1、vedio	//视频
+		controls 	//添加控件。
+		src/autoplay/width...
+		source元素	//链接不同的视频
+2、vedio/dom	
+		play()、pause()...
+3、audio
+4、拖放
+	<img draggable="true" />
+	ondragstart,ondragover,ondrop		//事件
+	dataTransfer.setData() 
+5、canvas
+	cxt.drawImage(imgObj,..)
+	cxt.fillRect()
+	cxt.stroke()
+	cxt.fill()
+	grad = cxt.createLinearGradient()	
+	grd.addColorStop()
+	cxt.fillStyle=grd;
+6、SVG
+7、地理定位
