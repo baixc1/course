c1655e65932eee2091cb65493192d152d7df59db
baixc <863562441@qq.com>
2018-05-03 18:59:00 +0800

    the course in May-three

--- python/note.js
+++ python/note.js
@@ -1,6 +1,15 @@
 Python 2.0
 1、默认ascii编码,转中文编码: #coding=utf-8 
 2、基础语法：交互式编程、脚本式编程、标识符、保留字符、行和缩进、多行语句(\)、引号、注释(#,'''/""")、空行、输出、代码组
-3、变量类型：Number、String([x,y]/+/*)、List、Tuple、Dictionary,   数据类型转换
+3、变量类型：Number、String([x,y]/+/*)、List、Tuple、Dictionary,(对象)   数据类型转换
 4、运算符	算术*、比较*、赋值*、位*、逻辑*、成员*(in)、身份*(is)、优先级
 5、条件语句(if elif else)、循环语句(while/for)、(break/continue/pass)
+6、Number	类型(Int/long integers/floating/complex)	类型转换  	math/cmath模块	随机数函数...
+7、字符串	访问值、更新、转义字符、运算符、格式化、内建函数
+8、列表		访问、更新(append/del)、操作符(len/+/*/in)、函数(cmp/len/max/min/list)/方法(append/count/extend/index...)
+9、元组		访问、修改(组合)、删除、运算符、内置函数(cmp/len/max/min/tuple)
+10、字典	访问、修改、删除、键特征(覆盖/不可改)、内置函数(cmp/len/str)、方法(clear/copy...)
+11、日期和时间	格式化时间元组(time.asctime( time.localtime(time.time()) ))  格式化(%y/m/...) 	日历(模块函数)	Time模块内置函数(time.fn)
+12、函数	定义(def)	参数(指针/(不)可变类型)(必备*/关键字*/缺省*/不定长*)	匿名函数(lambda表达式)  	return语句		作用域
+13、模块(Module)	import语句  from...import(*)	搜索路径(当前->shell->default)(sys.path)	PYTHONPATH环境变量	命名空间和作用域 	函数(dir/globals/locals)
+					包(__init__.py)(package.sub)
