795400dec988091443e5e8a0348d014bd6c25968
baixiaocheng <863562441@qq.com>
2018-05-28 23:50:06 +0800

    the course in 5.28 night

---
+++ webpack/runoob.js
@@ -0,0 +1,12 @@
+*Webpack 
+1、前端资源加载/打包工具		//模块依赖	静态分析
+2、安装(4.x)
+	- node.js
+	- npm install webpack -g		//全局安装webpack
+	- npm init			//package.json 
+	- ./src/index.js		//入口文件	
+	- package.json中scripts				//简化命令
+		npm run dev		//"dev":"webpack --mode development"
+		npm run build		//"build":"webpack --mode production"
+	- dist目录			//main.js文件
+			
