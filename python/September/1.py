600f6b7b0236b22b5f85956992b1dfea0c44709d
wsadc <863562441@qq.com>
2018-09-07 10:13:47 +0800

    python demo

--- python/September/1.py
+++ python/September/1.py
@@ -1,8 +1,20 @@
+import random
+secret = random.randint(1,10)
 print("-------------i love python--------------")
 temp = input("猜数字")
 guess = int(temp)
-if guess == 9:
-    print("true")
-else:
-    print("false")
+
+if guess == secret:
+        print("true")
+
+while guess != secret:
+    if guess == secret:
+        print("true")
+    else :
+        if guess > secret:
+            print("big")
+        else:
+            print("small")
+        temp = input('重输')
+        guess = int(temp)
 print("over")
