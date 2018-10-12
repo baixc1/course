748ae6042eb7e4709034fe8e1fa74717df4db4d3
wsadc <863562441@qq.com>
2018-10-12 18:31:19 +0800

    move add

--- dd-program/movable-area/pages/addText/addText.js
+++ dd-program/movable-area/pages/addText/addText.js
@@ -1,21 +1,21 @@
 //非页面data数据
-let isMoving = false    //是否正在拖动文字
 let originalImgWidth = 0    //原图宽，单位px
 let originalImgHeight = 0 
 let pageImgWidth = 0      //页面展示图片的宽
 let pageImgHeight = 0
 let screenWidth = 0     //屏幕宽
 let screenRate = 1      //屏幕宽比标准宽(375px)
-let showImgWidth = 0      //页面展示的图片宽度
-let showImgHeight = 0
 let pageImgMaxHeight = 0 //页面展示图片的最大高度（375屏幕下的px值，需转化）
 let moveObj = null     //移动对象位置信息
 let moveViewWidth = 100  //移动对象的宽，单位px
-let moveViewHeight = 100
+let moveViewHeight = 40
 let moveViewLeft = 0    //移动对象的左侧最小值
 let moveViewTop = 0
 let top = 0           //滑块左上角坐标
 let left = 0
+let moveViewMaxHeight  //最大滑动高度
+let moveViewMaxWidth    //最大滑动宽度
+let inputValue       //input组件值
 
 //获取系统信息-宽
 dd.getSystemInfo({
@@ -28,17 +28,22 @@ dd.getSystemInfo({
 
 Page({
   data: {
-    url: 'https://news.static.dongtu.com/toutiao/gif-sina-com-cn/1530038611/430439.lossy.gif',  //被修改的图片
+    url: 'https://img.alicdn.com/tfs/TB1GvVMj2BNTKJjy0FdXXcPpVXa-520-280.jpg',  //被修改的图片
     left: 0,
     top: 0,
+    showImgWidth: 0,    //显示图片的宽
+    showImgHeight: 0,
+    focus: false,   //是否获取了焦点
+    isShowInput: true,    //隐藏输入框
+    textValue: '请输入文字',    //text组件值
+    inputValue: '',       //input值（页面显示）
   },
-  //拖动文字-触摸动作开始
+  // 拖动文字-触摸动作开始
   touchStart(){
-    isMoving = true
+    moveObj = null        //清空触摸位置
   },
   //拖动文字-触摸后移动
   touchMove(e){
-    dd.stopPullDownRefresh()
     let d = this.data
     let curMoveObj = e.changedTouches[0]
     if(!moveObj){
@@ -54,44 +59,31 @@ Page({
 
       left += moveX
       top += moveY
-      if(d.top >= moveViewTop && d.top <= showImgHeight + moveViewTop - moveViewHeight){    //纵坐标范围
-        while(top < moveViewTop){    //newLeft和left可能相差过大
-          top += 1
+      if(d.top >= moveViewTop && d.top <= moveViewMaxHeight){    //纵坐标范围
+        if(top > moveViewMaxHeight){
+          top = moveViewMaxHeight
         }
-        while(top > showImgHeight + moveViewTop - moveViewHeight){
-          top -= 1
+        if(top < moveViewTop){
+          top = moveViewTop
         }
         this.setData({
           top: top,
         })
         moveObj.clientY = curMoveObj.clientY
       }
-      if(d.left >= moveViewLeft && d.left <= showImgWidth + moveViewLeft - moveViewWidth){   //实际横坐标范围
-        while(left < moveViewLeft){    //newLeft和left可能相差过大
-          left += 1
+      if(d.left >= moveViewLeft && d.left <= moveViewMaxWidth){   //实际横坐标范围
+        if(left > moveViewMaxWidth){
+          left = moveViewMaxWidth
         }
-        while(left > showImgWidth + moveViewLeft - moveViewWidth){
-          left -= 1
+        if(left < moveViewLeft){
+          left = moveViewLeft
         }
         this.setData({
           left: left
         })
         moveObj.clientX = curMoveObj.clientX
       }
-      // this.setData({
-      //   top: newTop,
-      //     left: newLeft
-      // })
-      // moveObj = curMoveObj
     }
-  },
-  //拖动文字-触摸动作结束
-  touchEnd(){
-    isMoving = false
-  },
-  //拖动文字-触摸动作被打断，如来电提醒，弹窗
-  touchCancel(){
-    this.touchEnd()
   },
   //需加字的图片加载完成
   modifyImgLoad(e){
@@ -102,6 +94,8 @@ Page({
     let originalRate = originalImgWidth / originalImgHeight   //原始图片宽高比
     screenRate = screenWidth / pageImgMaxHeight         //屏幕宽比230px的对应值
 
+    let showImgWidth = 0      //页面展示的图片宽度
+    let showImgHeight = 0
     if (originalRate > screenRate) {    //长图
       showImgWidth = screenWidth
       showImgHeight = screenWidth / originalRate
@@ -112,11 +106,78 @@ Page({
     
     moveViewLeft = (screenWidth - showImgWidth) / 2
     moveViewTop = (pageImgMaxHeight - showImgHeight) /2
+  
     this.setData({
       left: moveViewLeft,
-      top: moveViewTop
+      top: moveViewTop,
+      showImgWidth: showImgWidth,
+      showImgHeight: showImgHeight
     })
+
     left = moveViewLeft
     top = moveViewTop
-  }
+
+    moveViewMaxHeight = showImgHeight + moveViewTop - moveViewHeight
+    moveViewMaxWidth = showImgWidth + moveViewLeft - moveViewWidth
+  },
+  //input失去焦点
+  bindBlur(){
+    let textValue = inputValue || '请输入文字'
+    this.setData({
+      isShowInput: false,
+      textValue: textValue,
+      inputValue: inputValue,
+      focus: false
+    }) 
+  },
+  //点击文字
+  bindTextTap(){
+    this.setData({
+      isShowInput: true
+    },()=>{
+      this.setData({
+        focus: true
+      })
+    })
+  },
+  //input输入
+  bindInput(e){
+    inputValue = e.detail.value //输入值
+  },
+  //生成图片
+  generate(){
+    let d = this.data
+    let url = d.url
+    let context = dd.createCanvasContext('myCanvas')
+    context.drawImage(url,0,0,d.showImgWidth,d.showImgHeight)
+
+    context.setFontSize(20)
+    context.setTextBaseline('top')
+    context.fillText(inputValue,0,0)
+
+    context.draw()
+    setTimeout(()=>{
+      context.toTempFilePath({
+        success(res){
+          console.log(res)
+          dd.previewImage({
+            urls: [res.apFilePath]
+          })
+          dd.saveImage({
+            url: res.apFilePath,
+            success(){
+              
+            },
+            complete(data){
+              console.log(data)
+              dd.alert({
+                title: JSON.stringify(data)
+              })
+            }
+          })
+        }
+      })
+    },100)
+    
+  },
 });
