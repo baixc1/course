b4b6153e0ad376961254f964dba4582880fef61d
baixc1 <863562441@qq.ocm>
2019-02-12 18:38:50 +0800

    add

--- react/demo1/reactpc/src/main.js
+++ react/demo1/reactpc/src/main.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './component/examples/Sidebar.jsx'
+import App from './component/examples/ModalGallery.jsx'
 
 ReactDOM.render(<App />, document.getElementById('app'))
