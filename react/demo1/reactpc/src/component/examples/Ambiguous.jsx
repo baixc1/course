5e92848da033c8a56c0c31d20c4c9f8868da605b
baixc1 <863562441@qq.ocm>
2019-02-12 10:00:17 +0800

    react router4 demo

---
+++ react/demo1/reactpc/src/component/examples/Ambiguous.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+function AmbiguousExample() {
+    return (
+        <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/about">About Us (static)</Link>
+                    </li>
+                    <li>
+                        <Link to="/company">Company (static)</Link>
+                    </li>
+                    <li>
+                        <Link to="/kim">Kim (dynamic)</Link>
+                    </li>
+                    <li>
+                        <Link to="/chris">Chris (dynamic)</Link>
+                    </li>
+                </ul>
+
+                {/*
+          Sometimes you want to have a whitelist of static paths
+          like "/about" and "/company" but also allow for dynamic
+          patterns like "/:user". The problem is that "/about"
+          is ambiguous and will match both "/about" and "/:user".
+          Most routers have an algorithm to decide for you what
+          it will match since they only allow you to match one
+          "route". React Router lets you match in multiple places
+          on purpose (sidebars, breadcrumbs, etc). So, when you
+          want to clear up any ambiguous matching, and not match
+          "/about" to "/:user", just wrap your <Route>s in a
+          <Switch>. It will render the first one that matches.
+      */}
+                {/* <Switch> */}
+                <Route path="/about" component={About} />
+                <Route path="/company" component={Company} />
+                {/* match url start with / */}
+                <Route path="/:user" component={User} />
+                {/* match all */}
+                <Route path="/" component={Home} />
+                {/* </Switch> */}
+            </div>
+        </Router>
+    );
+}
+
+function About() {
+    return <h2>About</h2>;
+}
+function Home() {
+    return <h2>Home</h2>;
+}
+
+function Company() {
+    return <h2>Company</h2>;
+}
+
+function User({ match }) {
+    return (
+        <div>
+            <h2>User: {match.params.user}</h2>
+        </div>
+    );
+}
+
+export default AmbiguousExample;
