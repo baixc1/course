1e562046a3f4c65d9f93966485e044fe3767cec1
baixc1 <863562441@qq.ocm>
2019-04-22 10:21:11 +0800

    c

--- C/structure/TSMatrix.c
+++ C/structure/TSMatrix.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
 #define number 3
-
 typedef struct {
     int i,j;
     int data;
@@ -9,36 +8,28 @@ typedef struct {
     triple data[number];
     int n,m,num;
 }TSMatrix;
-
 //输出存储的稀疏矩阵
 void display(TSMatrix M);
-
 int main() {
     TSMatrix M;
     M.m=3;
     M.n=3;
     M.num=3;
+
     M.data[0].i=1;
     M.data[0].j=1;
     M.data[0].data=1;
+
     M.data[1].i=2;
     M.data[1].j=3;
     M.data[1].data=5;
+
     M.data[2].i=3;
     M.data[2].j=1;
     M.data[2].data=3;
     display(M);
     return 0;
 }
-
-int num = 0;
-void pushData(TSMatrix M, int param[3]){
-    ++num;
-    M.data[num].i = param[0];
-    M.data[num].j = param[1];
-    M.data[num].data = param[2];
-}
-
 void display(TSMatrix M){
     for(int i=1;i<=M.n;i++){
         for(int j=1;j<=M.m;j++){
